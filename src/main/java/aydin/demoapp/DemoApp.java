464b344e0ef43961b733305be1218511e593bf94
ilknur <ilknur@137.125.155.32>
2022-05-10 11:26:20 -0400

    first push

---
+++ src/main/java/aydin/demoapp/DemoApp.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package aydin.demoapp;
+import java.util.Scanner;
+/**
+ *
+ * @author ilknur
+ */
+public class DemoApp {
+
+    public static void main(String[] args) {
+        
+        
+        Scanner s = new Scanner (System.in);
+        Player p = new Player();
+        int i = s.nextInt();
+        p.name = "ilknur";
+        System.out.println("Hello World!" + i + p);
+    }
+}
---
+++ src/main/java/aydin/demoapp/Player.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aydin.demoapp;
+
+/**
+ *
+ * @author ilknur
+ */
+public class Player {
+    public String name;
+    public String toString() { return name; } 
+}
